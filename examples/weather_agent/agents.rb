0fe53942d5d3d46fe978f96c405915bd08f28244
Grayson Chen <cgg5207@sina.com>
2024-10-26 20:36:05 +0800

    weather_agent

---
+++ examples/weather_agent/agents.rb
@@ -0,0 +1,62 @@
+
+OpenAI.configure do |config|
+  config.access_token = ENV['OPEN_ROUTER_ACCESS_TOKEN']
+  config.uri_base = "https://openrouter.ai/api/v1"
+end
+
+client = OpenAISwarm.new
+
+# Client chat parameters: {:model=>"gpt-4", :messages=>[{:role=>"system", :content=>"You are a helpful agent."}, {"role"=>"user", "content"=>"Do I need an umbrella today? I'm in chicago."}, {"role"=>"assistant", "content"=>nil, "refusal"=>nil, "tool_calls"=>[{"index"=>0, "id"=>"call_spvHva4SFuDfTUk57EhuhArl", "type"=>"function", "function"=>{"name"=>"get_weather", "arguments"=>"{\n  \"location\": \"chicago\"\n}"}}], :sender=>"Weather Agent"}, {:role=>"tool", :tool_call_id=>"call_spvHva4SFuDfTUk57EhuhArl", :tool_name=>"get_weather", :content=>"{\"location\":{},\"temperature\":\"65\",\"time\":\"now\"}"}], :tools=>[{:type=>"function", :function=>{:name=>"send_email", :description=>"", :parameters=>{:type=>"object", :properties=>{:recipient=>{:type=>"string"}, :subject=>{:type=>"string"}, :body=>{:type=>"string"}}, :required=>["recipient", "subject", "body"]}}}, {:type=>"function", :function=>{:name=>"get_weather", :description=>"Get the current weather in a given location. Location MUST be a city.", :parameters=>{:type=>"object", :properties=>{:location=>{:type=>"string"}, :time=>{:type=>"string"}}, :required=>["location"]}}}], :stream=>false, :parallel_tool_calls=>true}
+def get_weather(location, time= Time.now)
+  # Get the current weather in a given location. Location MUST be a city.
+  { location: location, temperature: "65", time: time }.to_json
+end
+
+def send_email(recipient, subject, body)
+  puts "Sending email..."
+  puts "To: #{recipient}"
+  puts "Subject: #{subject}"
+  puts "Body: #{body}"
+  "Sent!"
+end
+
+function_instance_send_email = OpenAISwarm::Transfer.new(
+  transfer_agent: Proc.new { |recipient, subject, body| send_email(recipient, subject, body) },
+  transfer_name: 'send_email',
+)
+
+function_instance_get_weather = OpenAISwarm::Transfer.new(
+  transfer_agent: Proc.new { |location, time| get_weather(location, time="now") },
+  transfer_name: 'get_weather',
+  description: 'Get the current weather in a given location. Location MUST be a city.'
+)
+
+weather_agent = OpenAISwarm::Agent.new(
+  name: "Weather Agent",
+  instructions: "You are a helpful agent.",
+  functions: [
+    function_instance_send_email,
+    function_instance_get_weather
+  ]
+)
+
+msg1 = "Do I need an umbrella today? I'm in chicago."
+# model: "gpt-4",
+# return: The current temperature in Chicago is 65 degrees. It doesn't look like you'll need an umbrella today!
+msg2 = "Tell me the weather in London."
+# return: The current temperature in London is 65°F.
+response = client.run(
+  messages: [{"role" => "user", "content" => msg2}],
+  agent: weather_agent,
+  debug: true,
+)
+# print(response.messages[-1]["content"])
+
+
+response = client.run(
+  messages: [{"role" => "user", "content" => "What is the time right now?",}],
+  agent: weather_agent,
+  debug: true,
+)
+# p response.messages[-1]["content"]
+# return: I'm sorry for the confusion, but as an AI, I don't have the ability to provide real-time information such as the current time. Please check the time on your device.
