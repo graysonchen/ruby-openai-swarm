f89b200b0de0332bd40e39a12267c2bfbf89314b
Grayson Chen <cgg5207@sina.com>
2024-11-28 02:13:25 +0800

    can pass custom_parameters into function call parameters

--- lib/ruby-openai-swarm/function_descriptor.rb
+++ lib/ruby-openai-swarm/function_descriptor.rb
@@ -1,10 +1,13 @@
 module OpenAISwarm
   class FunctionDescriptor
-    attr_reader :target_method, :description
+    attr_reader :target_method,
+                :description,
+                :parameters
 
-    def initialize(target_method:, description: '')
+    def initialize(target_method:, description: '', parameters: {})
       @target_method = target_method.is_a?(Method) ? target_method : method(target_method)
       @description = description
+      @parameters = parameters
     end
   end
 end
