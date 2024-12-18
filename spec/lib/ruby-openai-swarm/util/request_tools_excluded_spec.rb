9dab491e1c16f1df5e253fa3f84569e6fe7bba9e
Grayson Chen <cgg5207@sina.com>
2024-12-18 22:53:37 +0800

    add test for prevent_agent_reentry

--- spec/lib/ruby-openai-swarm/util/request_tools_excluded_spec.rb
+++ spec/lib/ruby-openai-swarm/util/request_tools_excluded_spec.rb
@@ -49,5 +49,36 @@ RSpec.describe OpenAISwarm::Util do
       filtered = described_class.request_tools_excluded(tools, ['non_existent_tool'], true)
       expect(filtered).to eq(described_class.symbolize_keys_to_string(tools))
     end
+
+    context 'when prevent_agent_reentry is false' do
+      it 'returns original tools array regardless of tool_names' do
+        filtered = described_class.request_tools_excluded(tools, ['custom_analytics_agent'], false)
+        expect(filtered).to eq(tools)
+      end
+
+      it 'returns original tools array even with multiple tool names' do
+        filtered = described_class.request_tools_excluded(
+          tools,
+          ['custom_analytics_agent', 'conversion_funnel_analytics_agent'],
+          false
+        )
+        expect(filtered).to eq(tools)
+      end
+    end
+
+    context 'when prevent_agent_reentry is true' do
+      it 'filters out specified tools' do
+        filtered = described_class.request_tools_excluded(tools, ['custom_analytics_agent'], true)
+        expect(filtered.length).to eq(1)
+        expect(filtered[0]['function']['name'].to_s).to eq('conversion_funnel_analytics_agent')
+      end
+    end
+
+    context 'when tools array is empty' do
+      it 'returns nil regardless of prevent_agent_reentry value' do
+        expect(described_class.request_tools_excluded([], ['some_tool'], true)).to be_nil
+        expect(described_class.request_tools_excluded([], ['some_tool'], false)).to be_nil
+      end
+    end
   end
 end
